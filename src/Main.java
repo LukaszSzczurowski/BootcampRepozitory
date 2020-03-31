fa271fcca7ebef9de3d262a17cc26c095f0c6baa
Łukasz Szczurowski <lszczurowski0@gmail.com>
2020-03-31 20:11:01 +0200

    Dodałem out do .ignore oraz wyświetliłem zadane zdanie

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Uczę");
+        System.out.println("się");
+        System.out.println("programowania");
     }
 }
